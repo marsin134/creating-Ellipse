7a552e11e998e9613a5038bf22c94e7fe12c18d4
marsin <lampetow555@gmail.com>
2023-11-18 19:02:03 +0300

    creating Ellipse

---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5 import uic
+from random import randint
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+
+
+class Ellipse(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        self.update()
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        qp.setBrush(QColor(255, 255, 0))
+        x, y = randint(100, 600), randint(100, 450)
+        diametr = randint(10, 75)
+        qp.drawEllipse(x, y, diametr, diametr)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Ellipse()
+    ex.show()
+    sys.exit(app.exec_())
