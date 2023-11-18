b9d8ab9c2cd86f754811fe560a02c1d6dda63c92
marsin <lampetow555@gmail.com>
2023-11-18 19:06:58 +0300

    random colors Ellipse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5 import uic
+from PyQt5 import QtCore
 from random import randint
+from UI import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
 
@@ -8,9 +9,14 @@ from PyQt5.QtGui import QPainter, QColor
 class Ellipse(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        Ui_MainWindow.setupUi(self, self)
         self.start = False
         self.pushButton.clicked.connect(self.run)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "! ЖМИ !"))
 
     def run(self):
         self.start = True
@@ -20,7 +26,7 @@ class Ellipse(QMainWindow):
         if self.start:
             qp = QPainter()
             qp.begin(self)
-            qp.setBrush(QColor(255, 255, 0))
+            qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
             x, y = randint(100, 600), randint(100, 450)
             diametr = randint(10, 75)
             qp.drawEllipse(x, y, diametr, diametr)
